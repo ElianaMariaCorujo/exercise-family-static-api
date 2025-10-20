253441a05cb8ed5fd70ca12713a5206443eee040
Eliana Corujo <elianamariacorujo@gmail.com>
2025-10-20 22:18:07 +0000

    Ejercico terminado

--- src/app.py
+++ src/app.py
@@ -5,8 +5,8 @@ import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from datastructures import FamilyStructure
-#from models import Person
+# Importamos la estructura de datos
+from datastructures import FamilyStructure 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -25,18 +25,60 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# --- INICIO DE NUESTROS ENDPOINTS ---
+
+# 1. Endpoint: [GET] /members (Obtener todos)
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_all_members():
 
-    # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    
+    return jsonify(members), 200
+
+# 2. Endpoint: [GET] /member/<int:member_id> (Obtener uno)
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_one_member(member_id):
+   
+    member = jackson_family.get_member(member_id)
+    
+    if member:
+    
+        return jsonify(member), 200
+    else:
+        
+        return jsonify({"message": "Member not found"}), 404
 
 
-    return jsonify(response_body), 200
+@app.route('/member', methods=['POST'])
+def add_new_member():
+   
+    body = request.get_json(silent=True)
+
+   
+    if body is None:
+        return jsonify({"message": "Bad request: JSON body is missing"}), 400
+    
+    required_fields = ['id', 'first_name', 'age', 'lucky_numbers']
+    if not all(field in body for field in required_fields):
+        return jsonify({"message": "Bad request: Missing required fields"}), 400
+
+    jackson_family.add_member(body)
+
+    return jsonify({"message": "Member added"}), 200
+
+# 4. Endpoint: [DELETE] /member/<int:member_id> (Eliminar uno)
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_one_member(member_id):
+    success = jackson_family.delete_member(member_id)
+    
+    if success:
+        
+        return jsonify({"done": True}), 200
+    else:
+       
+        return jsonify({"message": "Member not found"}), 404
+
+# --- FIN DE NUESTROS ENDPOINTS ---
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
